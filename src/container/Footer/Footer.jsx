58e3866f6a954005d69c12f2a60c3584aea64955
Lucas Samuel Pereyra <lucaspereyra470@gmail.com>
2023-02-16 17:26:50 -0300

    contact section language finished

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { LangContext } from '../../App';
 import AboutImage from '../../assets/images/AboutImage';
 import { AppWrap, MotionWrap } from '../../wrapper';
 
 import emailjs from 'emailjs-com';
 
 const Footer = () => {
+    const langContext = useContext(LangContext);
+    const lang = langContext.contact;
+
     const [message, setMessage] = useState(null);
+
+    const input = lang.input;
 
     const handleSubmitEmail = (event) => {
         event.preventDefault();
@@ -28,7 +34,7 @@ const Footer = () => {
     return (
         <>
             <div className="app__container">
-                <h2 className="head-text">¡Hablemos!</h2>
+                <h2 className="head-text">{lang.title}</h2>
 
                 <div className="mt-16 flex w-full flex-row gap-4 max-lg:flex-col max-lg:gap-y-12 lg:gap-0 2xl:gap-4">
                     <div className="h-[180px] min-h-[100px] w-full min-w-[200px] lg:h-[300px] lg:w-[400px] xl:h-[400px] xl:w-[700px]">
@@ -47,34 +53,34 @@ const Footer = () => {
                             <input
                                 className="rounded-md px-6 py-3 transition-all hover:shadow-md focus:shadow-lg focus:outline-none"
                                 type="text"
-                                placeholder="Nombre"
                                 id="name"
                                 name="name"
+                                placeholder={input.namePlaceholder}
                                 required
                             />
                             <input
                                 className="rounded-md px-6 py-3 transition-all hover:shadow-md focus:shadow-md focus:outline-none"
                                 type="email"
-                                placeholder="Email"
                                 id="email"
                                 name="email"
+                                placeholder="Email"
                                 required
                             />
                             <textarea
                                 className="h-[200px] resize-none rounded-md px-6 py-3 transition-all hover:shadow-md focus:shadow-md focus:outline-none"
                                 name="message"
                                 id="message"
-                                placeholder="Descripción"
+                                placeholder={input.descriptionPlaceholder}
                                 required
                             ></textarea>
                             <button className="mx-auto w-[50%] rounded-md bg-emerald-500 py-2 text-white transition-colors hover:bg-emerald-700/90 lg:w-[35%] 2xl:w-[25%]">
-                                Enviar
+                                {lang.btn}
                             </button>
                         </form>
                     </div>
                 </div>
                 <div className="my-16 text-center lg:mt-16">
-                    <p>Hecho con ❤️ por Lucas Pereyra &copy;</p>
+                    <p>{lang.autorText} &copy;</p>
                 </div>
             </div>
         </>
